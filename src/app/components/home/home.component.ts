f25d4a4095bb9db6e781d5dd5a273b6b20651a13
eren5854 <ihsanerendelibas@hotmail.com>
2025-03-04 03:48:33 +0300

    .

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  isModalActive:boolean = false;
+
+
+
+  activeModal(){
+    this.isModalActive = !this.isModalActive;
+  }
+}
